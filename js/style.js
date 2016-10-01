b1abf2e4bec56bf2e067b5b8fa9151c38bd0e175
Kapil Verma <kapv89@gmail.com>
2016-10-01 07:13:35 +0530

    flowapp site

---
+++ js/style.js
@@ -0,0 +1,34 @@
+function sizeBanner() {
+  $('div.banner').css({
+    width: $(window).width(),
+    height: $(window).height() * 0.9,
+    overflow: 'hidden'
+  })
+}
+
+function toggleAppDownloadFloaterVisibility() {
+  if ($(window).scrollTop() > $('div.banner:first').height() - 300) {
+    $('div.download-floater').show();
+  } else {
+    $('div.download-floater').hide();
+  }
+}
+
+function toggleAppLogoFloaterVisibility() {
+  if ($(window).scrollTop() > $('div.banner:first').height() - 300) {
+    $('div.logo-floater').show();
+  } else {
+    $('div.logo-floater').hide();
+  }
+}
+
+$(function () {
+  $(window).on('resize', sizeBanner);
+  sizeBanner();
+
+  $(window).on('scroll', toggleAppDownloadFloaterVisibility);
+  toggleAppDownloadFloaterVisibility();
+
+  $(window).on('scroll', toggleAppLogoFloaterVisibility);
+  toggleAppLogoFloaterVisibility();
+});
